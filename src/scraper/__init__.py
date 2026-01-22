3c6b4be8fbed5b9024c8fb2a2bed9be5a0ef76c8
wahab1436 <abdul00wahab1000@gmail.com>
2026-01-22 20:36:26 +0500

    Update __init__.py

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,6 +1,7 @@
+
 """
 Scraper module for collecting financial news data.
 """
-from src.scraper.rss_news_scraper import RSSNewsScraper, scrape_and_save
+from src.scraper.investing_scraper import RSSNewsScraper, scrape_and_save
 
 __all__ = ['RSSNewsScraper', 'scrape_and_save']
